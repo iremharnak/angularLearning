7b87d9809a07b6a60a3ae9a3546c4580fa89bd12
iremharnak <iremharnak@users.noreply.github.com>
2022-11-22 11:55:13 -0500

    learning http req

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy} from "@angular/core";
+import { Subscription } from "rxjs";
 import { IProduct } from './product';
 import { ProductService } from "./product.service";
 @Component({
@@ -6,12 +7,14 @@ import { ProductService } from "./product.service";
   templateUrl:'./product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean= false;
+  errorMessage: string= '';
+  sub!: Subscription;
   private _listFilter: string = '';
   get listFilter(): string {
     return this._listFilter;
@@ -36,9 +39,16 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
-    
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    }); 
+  }
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
   onRatingClicked(message: string): void {
     this.pageTitle = 'Product List: ' + message;
--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,31 +1,35 @@
+import { HttpClient , HttpErrorResponse} from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { IProduct } from "./product";
+import { Observable, catchError, tap, throwError } from "rxjs";
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  getProducts(): IProduct[] {
-    return [
-      {
-        "productId": 8,
-        "productName": "Saw",
-        "productCode": "TBX-0022",
-        "releaseDate": "May 15, 2021",
-        "description": "15-inch steel blade hand saw",
-        "price": 11.55,
-        "starRating": 3.7,
-        "imageUrl": "assets/images/saw.png"
-      },
-      {
-        "productId": 10,
-        "productName": "Video Game Controller",
-        "productCode": "GMG-0042",
-        "releaseDate": "October 15, 2020",
-        "description": "Standard two-button video game controller",
-        "price": 35.95,
-        "starRating": 4.6,
-        "imageUrl": "assets/images/xbox-controller.png"
-      }
-    ]
+  private productUrl = 'api/products/products.json';
+  constructor(private http: HttpClient){
+
+  }
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productUrl)
+    .pipe(
+      tap(data => console.log('All', JSON.stringify(data))),
+    catchError(this.handleError)
+    );
+  }
+  private handleError(err: HttpErrorResponse){
+    // in a real world app, we may send the server to some remote logging infrastructure instead of just logging it to the console
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network err occured. Handle it accordingly.
+      errorMessage = `An error occured: ${err.error.message}`;
+    } else {
+      // the backend returned an usuccessful response code.
+      // the response body may contain clues as to what went wrong,
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(() => errorMessage);
   }
 }
